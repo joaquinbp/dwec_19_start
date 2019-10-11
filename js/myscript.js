6b1c33a09d1a7311ae9c1d7e39c0159e5246982d
Joaquin Bono <joaquinbono.18@campuscamara.es>
2019-10-11 20:14:07 +0200

    [JS] Operations

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,68 @@
-alert("Hello World JS");
+//alert("Hello World JS");
+
+// console.log("Hello World");
+
+// var table="Normal Table";
+// let chair="One chair";
+// console.log(table);
+// console.log(chair);
+
+// let testBoolean=true;
+// console.log(testBoolean);
+
+// let testString='text';
+// console.log(testString);
+
+// let testNumber=20;
+// console.log(testNumber);
+
+// let testBooleanObject=new Boolean(true);
+// console.log(testBooleanObject);
+
+// let testNumberObject=new Number(10);
+// console.log(testNumberObject);
+
+// let testStringObject=new String('text');
+// console.log(testStringObject);
+
+// let name="John";
+// let surname="Doe";
+// let question="How are you "+name+" "+surname+"?";
+// console.log(question);
+
+// let age=23;
+// let question2=`How old is ${name} ${surname}?`;
+// let answer="He is "+age+" years old";
+// console.log(question2);
+// console.log(answer);
+
+// let a=2;
+// let b=5;
+// let division=a/b;
+// console.log(division);
+// let expo= a**b;
+// let incr=++a;
+// let decr=--b;
+// console.log(expo);
+// console.log(incr);
+// console.log(decr);
+
+// let x=5;
+// let y=6;
+// console.log(x+=5);
+// console.log(x-=5);
+// console.log(x*=3);
+// console.log(y/=2);
+
+// let testBoolean=true;
+// let testNumber=20;
+// let testString="Hello";
+// let testBooleanObject=new Boolean(true);
+// console.log(typeof(testNumber));
+//  console.log(typeof(testBooleanObject));
+
+let testNull=null;
+console.log(typeof(testNull));
+
+let testUndefined;
+console.log(testUndefined);
